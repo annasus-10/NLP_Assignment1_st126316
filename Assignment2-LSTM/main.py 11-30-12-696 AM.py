cd240a3db8f35499838b0c824060ab30739f2567
Thet Su Sann <thetsusann@Susans-MacBook-Pro.local>
2026-02-15 21:34:42 +0700

    thought i did but things did not get commited

---
+++ Assignment2-LSTM/main.py 11-30-12-696 AM.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from assignment2-lstm!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment3-Machine Translation/main.py 12-13-45-527 PM.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from assignment3-machine-translation!")
+
+
+if __name__ == "__main__":
+    main()
